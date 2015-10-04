a481bd8335840eef652d05019ad8b3d47ae35dd7
Mikhail Fursov <mike.fursov@gmail.com>
2015-10-03 23:43:05 -0700

    Adding more javadocs

--- src/main/java/com/github/sypexgeo/model/value/IntValue.java
+++ src/main/java/com/github/sypexgeo/model/value/IntValue.java
@@ -1,25 +1,43 @@
 package com.github.sypexgeo.model.value;
 
 /**
- *
+ * 'Int' value objects. Wraps non-nullable string value & implements hashCode(), equals() & toString methods.
  */
 public abstract class IntValue {
+
+    /**
+     * The value wrapped by class.
+     */
     public final int value;
 
+    /**
+     * Initializes new IntValue object.
+     *
+     * @param value - any 'int' value.
+     */
     public IntValue(int value) {
         this.value = value;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         return o == this || (o instanceof IntValue && value == ((IntValue) o).value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Integer.hashCode(value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return getClass().getSimpleName() + ":" + value;
--- src/main/java/com/github/sypexgeo/model/value/StringValue.java
+++ src/main/java/com/github/sypexgeo/model/value/StringValue.java
@@ -2,27 +2,48 @@ package com.github.sypexgeo.model.value;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 /**
- *
+ * String value objects. Wraps non-nullable string value & implements hashCode(), equals() & toString methods.
  */
 public abstract class StringValue {
+    /**
+     * The value wrapped by class.
+     */
     @NotNull
     public final String value;
 
+    /**
+     * Initializes new StringValue object.
+     *
+     * @param value - non nul string value.
+     * @throws NullPointerException if provided String value is null.
+     */
     public StringValue(@NotNull String value) {
+        Objects.requireNonNull(value);
         this.value = value;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         return o == this || (o instanceof StringValue && value.equals(((StringValue) o).value));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return getClass().getSimpleName() + ":" + value;
